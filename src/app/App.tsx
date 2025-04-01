6c755711fbc8b55222f33c6439cb23c1dfb02765
Ivan Belikov <belikov.workmail@gmail.com>
2025-04-01 15:39:20 +0300

    queryClientProiver aded

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,19 @@
-import { Fragment } from 'react'
-
 import { RouterProvider } from 'react-router'
 import { router } from './router'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@base/shared/lib/queryClient'
 
 import { ThemeProvider } from '@base/shared/providers/theme'
 import { CssBaseline } from '@mui/material'
 
 export const App = () => {
   return (
-    <Fragment>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <CssBaseline />
         <RouterProvider router={router}></RouterProvider>
       </ThemeProvider>
-    </Fragment>
+    </QueryClientProvider>
   )
 }
