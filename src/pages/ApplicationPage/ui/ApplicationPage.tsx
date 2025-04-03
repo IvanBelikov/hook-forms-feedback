2a36709c7ac36370d97687721623bf0fa9d93fd0
Ivan Belikov <belikov.workmail@gmail.com>
2025-04-03 22:32:50 +0300

    application page changed

--- src/pages/ApplicationPage/ui/ApplicationPage.tsx
+++ src/pages/ApplicationPage/ui/ApplicationPage.tsx
@@ -1,3 +1,29 @@
-import { Fragment } from 'react'
+import { ApplicationForm } from '@base/features/applications'
 
-export const ApplicationPage = () => <div></div>
+import { Container, Paper, Button, Typography } from '@mui/material'
+import { ArrowBack } from '@mui/icons-material'
+import { useNavigate } from 'react-router'
+
+export const ApplicationPage = () => {
+  const navigate = useNavigate()
+  const handleBackButton = () => navigate(-1)
+
+  return (
+    <Container maxWidth="md" sx={{ py: 4 }}>
+      <Paper elevation={3} sx={{ p: 4 }}>
+        <Button
+          sx={{ mb: 2 }}
+          onClick={handleBackButton}
+          startIcon={<ArrowBack />}
+          variant="outlined"
+        >
+          Back
+        </Button>
+        <Typography variant="h4" component="h1">
+          New conference report application
+        </Typography>
+        <ApplicationForm />
+      </Paper>
+    </Container>
+  )
+}
